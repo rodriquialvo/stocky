aa2fd1c48d57e053c82b6e27a1e66dae53c0ec3c
Jose Rodriguez <jrodriguez@denwaip.com>
2024-09-24 21:50:08 -0300

    fix generator

--- generator/templates/simple-component/__ComponentName__.tsx
+++ generator/templates/simple-component/__ComponentName__.tsx
@@ -1,16 +1,15 @@
-import {View} from 'react-native';
+import {Box, Text} from '@chakra-ui/react';
 import React, {FC} from 'react';
-import styles from './__ComponentName__.styles';
+import styles from './__ComponentName__.module.css';
 import {__ComponentName__Props} from './interfaces';
-import Text from '../../../src/components/Text/Text';
 
 //REMOVE
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const __ComponentName__: FC<__ComponentName__Props> = props => {
   return (
-    <View style={styles.root}>
+    <Box className={styles.root}>
       <Text>__ComponentName__</Text>
-    </View>
+    </Box>
   );
 };
 
