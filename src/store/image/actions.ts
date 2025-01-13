9bf5478959d98e8f112ea50c6939bc3f6110674e
Jose Rodriguez <jrodriguez@denwaip.com>
2025-01-13 12:59:52 -0300

    fix upload images

--- src/store/image/actions.ts
+++ src/store/image/actions.ts
@@ -10,12 +10,30 @@ export const ImageAction = () => {
 
   const setStatus = useImageStore(state => state.setStatus)
   const imageService = useAPIImageService()
+
+  const base64ToBlob = (base64, mimeType) => {
+    const byteCharacters = atob(base64.split(',')[1]); // Eliminar el prefijo data:image/jpeg;base64,
+    const byteArrays = [];
+  
+    for (let offset = 0; offset < byteCharacters.length; offset += 1024) {
+      const byteSlice = byteCharacters.slice(offset, offset + 1024);
+      const byteNumbers = new Array(byteSlice.length);
+      
+      for (let i = 0; i < byteSlice.length; i++) {
+        byteNumbers[i] = byteSlice.charCodeAt(i);
+      }
+      byteArrays.push(new Uint8Array(byteNumbers));
+    }
+  
+    return new Blob(byteArrays, { type: mimeType });
+  }
+
   const createNewImageUrl = async (body: any) => {
 
     setStatus(getStartStatus());
     
     const formData:any = new FormData();
-    await formData.append('file', body);
+    await formData.append('file', base64ToBlob(body.data_url, body.file.type));
     
     try {
       const response = await imageService.postCreateNewImageUrl(formData);
