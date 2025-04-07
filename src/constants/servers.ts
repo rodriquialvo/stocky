b1ffb7f8b750b13915d0f66935187913da972eeb
Jose Rodriguez <jrodriguez@denwaip.com>
2025-04-07 20:17:05 -0300

    Update key isWholesalePackage to send correctly to backend

--- src/constants/servers.ts
+++ src/constants/servers.ts
@@ -2,8 +2,8 @@ const servers = {
   uat: 'https://....',
   qa: 'https://novavizija-rialex-api-qa.azurewebsites.net/api',
   // dev: 'https://stocky-3zn6.onrender.com/api',
-  dev: "https://stocky-318321636416.us-central1.run.app/api",
-  local: 'http://localhost:8080/api',
+  // dev: "https://stocky-318321636416.us-central1.run.app/api",
+  dev: 'http://localhost:8080/api',
 };
 
 export default servers;
